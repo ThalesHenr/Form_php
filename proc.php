483ca296dc387394eb8006b081fd34a5a4026c87
Thales Henrique <93166504+ThalesHenr@users.noreply.github.com>
2022-03-25 22:07:29 -0300

    Update proc.php
    Atualizando.

--- proc.php
+++ proc.php
@@ -4,12 +4,31 @@
 //$sexo = $_GET['sexo'];
 $nome = $_POST['nome'];
 $sexo = $_POST['sexo'];
+$senha = $_POST['senha'];
 
 
-echo "<p><strong> Nome: ".$nome."</strong></p>";
-echo "<p><strong> Sexo: ".$sexo."</strong></p>";
+//echo "<p><strong> Nome: ".$nome."</strong></p>";
+//echo "<p><strong> Sexo: ".$sexo."</strong></p>";
 //echo "<p><strong> senha: ".$_GET['senha']"</strong></p>";
-echo "<p><strong> ?:".$_REQUEST['nome']."</strong></p>";
+//echo "<p><strong> ?:".$_REQUEST['nome']."</strong></p>";
 
+//echo strlen($nome);//Quantidade de caracteres
+//echo "<br>";
+//echo (int)empty($nome);
+
+if (strlen($nome)==0){
+    //echo "campo obrigatório";
+    header ("location:aula.php?erro=Nome");
+}
+
+//if (strlen($sexo) ==0 ){
+    //echo "campo obrigatório";
+//}
+
+//senha mínimo 6 caracteres 
+
+if (strlen($senha)<=3){
+  echo "campo obrigatório";
+}
 
 ?>
